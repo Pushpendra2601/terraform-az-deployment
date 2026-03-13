6c806fba683ea4c427c624720b91db235227ae61
Pushpendra Vishwakarma <pushpendrav2601@gmail.com>
2026-03-13 17:15:31 +0530

    Initial Terraform Azure infrastructure

---
+++ terra-IaC/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "app_plan" {
+  name                = "week1-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.app_plan.id
+
+  site_config {
+    application_stack {
+      node_version = "18-lts"
+    }
+  }
+}
---
+++ terra-IaC/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  default = "terraform-devops-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "app_name" {
+  default = "terraform-devops-app"
+}
+
+variable "sql_admin_user" {
+  default = "sqladmin"
+}
+
+variable "sql_admin_password" {
+  default = "Password123!"
+}
